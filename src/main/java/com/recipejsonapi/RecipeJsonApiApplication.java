30be9d50604586515ad05ca1223a183ce34bea35
Seth Gustafson <sethrow1995@gmail.com>
2022-05-23 11:42:42 -0700

    Initial commit

---
+++ src/main/java/com/recipejsonapi/RecipeJsonApiApplication.java
@@ -0,0 +1,13 @@
+package com.recipejsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeJsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeJsonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipejsonapi/RecipeJsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipejsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeJsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
